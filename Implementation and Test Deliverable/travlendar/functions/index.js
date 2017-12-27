676868db74b32542998636614d3f9442f54f7cdf
andreataglia <drew.taglia@gmail.com>
2017-12-27 12:34:54 +0100

    travlendar: cloud function added -> first login now generates default preferences for the user. successfully tested

--- Implementation and Test Deliverable/travlendar/functions/index.js
+++ Implementation and Test Deliverable/travlendar/functions/index.js
@@ -1,26 +1,28 @@
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- * ...
- */
-
 // Import the Firebase SDK for Google Cloud Functions.
 const functions = require('firebase-functions');
 // Import and initialize the Firebase Admin SDK.
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
-
-
-// Adds a message that welcomes new users into the chat.
-exports.addWelcomeMessages = functions.auth.user().onCreate(event => {
-  const user = event.data;
-  console.log('A new user signed in for the first time.');
-  const fullName = user.displayName || 'Anonymous';
-
-  // Saves the new welcome message into the database
-  // which then displays it in the FriendlyChat clients.
-  return admin.database().ref('messages').push({
-    name: 'Firebase Bot',
-    photoUrl: '/images/firebase-logo.png', // Firebase logo
-    text: `${fullName} signed in for the first time! Welcome!` // Using back-ticks.
-  }).then(() => console.log('Welcome message written to database.'));
+exports.initUserPrefs = functions.auth.user().onCreate(event => {
+    const user = event.data;
+    const userid = user.uid;
+    console.log('A new user signed in for the first time');
+    return admin.database().ref('preferences/' + userid + '/break').set({
+        lowerBound: 600,
+        minTime: "60",
+        upperBound: 940
+    }).then(() => {
+        admin.database().ref('preferences/' + userid + '/travel').set({
+            allow_bike_sharing: true,
+            allow_car_sharing: true,
+            bike_disabled: false,
+            car_disabled: false,
+            disable_walking: false,
+            home: "Milan, Italy",
+            own_bike: true,
+            own_car: true,
+            public_transport_disabled: false,
+            reduce_carbon_footprint: false
+        }).then(() => console.log('User default preferences written to database'));
+    });
 });
