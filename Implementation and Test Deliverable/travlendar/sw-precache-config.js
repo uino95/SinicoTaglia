6ce89dbf3c8516f384974c73662bd3251cf5e950
andreataglia <drew.taglia@gmail.com>
2018-01-03 21:56:44 +0100

    ITD: version 1.0 ready. Travlendar: minor bug fixes

--- Implementation and Test Deliverable/travlendar/sw-precache-config.js
+++ Implementation and Test Deliverable/travlendar/sw-precache-config.js
@@ -1,17 +1,16 @@
 module.exports = {
     staticFileGlobs: [
-        '/index.html',
-        '/travlendar-app.html',
-        '/manifest.json',
-        '/bower_components/webcomponentsjs/webcomponents-lite.js',
-        '/images/*'
+        'manifest.json',
+        'bower_components/webcomponentsjs/webcomponents-lite.js',
+        'images/*'
     ],
-    externals: ['/'],
-    navigateFallback: '/index.html',
-    navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
-    ignoreUrlParametersMatching: [/^https:\/\/travlendar-plus\.firebaseapp\.com/],
     runtimeCaching: [{
-        urlPattern: /this\\.is\\.a\\.regex/,
-        handler: 'fastest'
+        urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
+        handler: 'fastest',
+        options: {
+            cache: {
+                name: 'webcomponentsjs-polyfills-cache'
+            }
+        }
     }]
 }
